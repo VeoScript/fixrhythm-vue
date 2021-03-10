2e7f4b3af16d81ae5966ee2a7a46cf97d683b9c3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-10 17:40:40 +0800

    added forgot-password and changed padding

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,32 @@
 <template>
-  <div class="h-screen flex items-center justify-center p-5 bg-gray-50">
-    <div class="block sm:hidden md:hidden lg:hidden">
-      <div class="text-4xl font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
-      <div class="text-xl w-3/4">Develop your skills in music, compose your song and make it public to the world.</div>
+  <div class="h-screen flex items-center justify-center p-20 bg-gray-50">
+    <div class="block xs:hidden sm:hidden md:hidden lg:hidden">
+      <div class="text-5xl mb-3 font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
+      <div class="text-xl">Develop your skills in music. <br/> Compose your song and make it public to the world.</div>
     </div>
     <div class="flex flex-col">
-      <div class="hidden sm:block md:block lg:block">
+      <div class="hidden xs:block sm:block md:block lg:block">
         <div class="text-4xl text-center font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
+        <div class="text-xs text-center mt-2">Poetry, Arts & Music</div>
       </div>
-      <div class="form-card shadow p-5">
+      <div class="form-card border shadow-lg p-10 mt-5 ml-10 mr-10 bg-white">
         <div class="form-control">
-          <div class="font-semibold mb-5">Sign in</div>
+          <div class="font-semibold text-3xl sm:text-base md:text-base lg:text-base mb-5">Sign in</div>
         </div>
         <div class="form-control w-full mb-2">
-          <input class="p-3 w-72" type="text" placeholder="Email" />
+          <input class="p-3 w-72 border-2 rounded-lg border-gray-300 focus:border-gray-500 focus:outline-none" type="text" placeholder="Email" />
         </div>
         <div class="form-control w-full mb-2">
-          <input class="p-3 w-72" type="password" placeholder="Password" />
+          <input class="p-3 w-72 border-2 rounded-lg border-gray-300 focus:border-gray-500 focus:outline-none" type="password" placeholder="Password" />
         </div>
         <div class="form-control w-full mb-2">
-          <button class="p-3 w-72">Login</button>
+          <button class="p-3 w-72 bg-gray-900 rounded-lg text-white hover:bg-gray-800 focus:outline-none">Sign in</button>
+        </div>
+        <div class="form-control w-full text-center mt-3 mb-3">
+          <nuxt-link class="text-sm text-gray-700 hover:underline" to="/forgot-password">Forgot Password?</nuxt-link>
+        </div>
+        <div class="form-control w-full text-center mb-2">
+          <hr />
         </div>
       </div>
     </div>
