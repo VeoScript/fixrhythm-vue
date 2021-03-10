d44be1c88d18f40db6c68dc96b9f63db932f7e72
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-10 16:58:16 +0800

    added login button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex items-center justify-center p-5 bg-gray-100">
+  <div class="h-screen flex items-center justify-center p-5 bg-gray-50">
     <div class="block sm:hidden md:hidden lg:hidden">
       <div class="text-4xl font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
       <div class="text-xl w-3/4">Develop your skills in music, compose your song and make it public to the world.</div>
@@ -8,7 +8,7 @@
       <div class="hidden sm:block md:block lg:block">
         <div class="text-4xl text-center font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
       </div>
-      <div class="form-card border-2 p-5">
+      <div class="form-card shadow p-5">
         <div class="form-control">
           <div class="font-semibold mb-5">Sign in</div>
         </div>
@@ -17,6 +17,9 @@
         </div>
         <div class="form-control w-full mb-2">
           <input class="p-3 w-72" type="password" placeholder="Password" />
+        </div>
+        <div class="form-control w-full mb-2">
+          <button class="p-3 w-72">Login</button>
         </div>
       </div>
     </div>
