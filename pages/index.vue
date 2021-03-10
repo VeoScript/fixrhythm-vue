927486d8846e297eaefbc28a5362917cbe5b34dc
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-10 18:34:05 +0800

    changed padding

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
         <div class="text-4xl text-center font-black uppercase"><span class="text-red-600">Fix</span>rhythm</div>
         <div class="text-xs text-center mt-2">Poetry, Arts & Music</div>
       </div>
-      <div class="form-card w-96 border shadow-lg p-10 mt-5 ml-10 mr-10 bg-white">
+      <div class="form-card w-96 border shadow-lg p-5 mt-5 ml-10 mr-10 bg-white">
         <div class="form-control">
           <div class="font-semibold text-2xl sm:text-base md:text-base lg:text-base mb-5">Log in</div>
         </div>
