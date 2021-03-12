36e9988c4d470226866fdc8d03ad3b63ba027c59
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-12 13:02:57 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,10 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    {
+      src: '~/plugins/vue2-perfect-scrollbar.js',
+      mode: 'client'
+    }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
