21764f8d14b0376cd86ae6037abade0f4761ddc3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-13 16:40:20 +0800

    import navbar.vue component

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="h-screen flex">
     <sidebar />
+    <navbar />
     <h5>This is Notifications</h5>
   </div>
 </template>
 
 <script>
 import Sidebar from '~/components/layouts/Sidebar.vue'
+import Navbar from '~/components/layouts/Navbar.vue'
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    Navbar
   }
 }
 </script>
