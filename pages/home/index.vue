671e74d6a8c8a242fa63a7d33dbfeb993cbe851e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-13 13:22:08 +0800

    add bottombar

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="h-screen flex">
     <sidebar />
+    <bottombar />
   </div>
 </template>
 
 <script>
-import Sidebar from '~/components/home/Sidebar.vue'
+import Sidebar from '~/components/layouts/Sidebar.vue'
+import Bottombar from '~/components/layouts/Bottombar.vue'
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    Bottombar
   }
 }
 </script>
