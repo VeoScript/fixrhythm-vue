0ad1439d3a70a704c35c5a9ea1c2456a425a7685
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-13 13:48:08 +0800

    remove bottombar component

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="h-screen flex">
     <sidebar />
-    <bottombar />
   </div>
 </template>
 
 <script>
 import Sidebar from '~/components/layouts/Sidebar.vue'
-import Bottombar from '~/components/layouts/Bottombar.vue'
 export default {
   components: {
-    Sidebar,
-    Bottombar
+    Sidebar
   }
 }
 </script>
