ee9eda645ea514421694bf7adb2a93102bd0e6b7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-13 15:05:43 +0800

    create poems/index.vue

---
+++ pages/poems/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex">
+    <sidebar />
+    <h5>This is Poems</h5>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/layouts/Sidebar.vue'
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
