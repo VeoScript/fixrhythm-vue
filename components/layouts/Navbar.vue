68489f7f88d2e4aabe552c6c7604477e738b5fbb
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-15 12:43:28 +0800

    add avatar and more-icon

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -2,9 +2,13 @@
   <div class="flex">
     <div class="fixed top-0 w-full">
       <nav class="top-0 w-full bg-gray-900 p-3 text-xs">
-        <ul class="flex text-left text-white text-lg font-bold">
+        <ul class="flex relative text-left text-white text-lg font-bold">
           <li>
             <h5 class="ml-5">{{ title }}</h5>
+          </li>
+          <li class="absolute flex items-center inset-y-0 right-0 space-x-3">
+            <img class="h-8 w-8 rounded-full ring-2 ring-gray-600" src="https://avatars.githubusercontent.com/u/26340308?s=460&u=f9dcea548505a99a2c873efb8b933aac0fe07abd&v=4" alt="account_avatar">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current text-gray-100" viewBox="0 0 24 24"><path d="M12 18c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3z"/></svg>
           </li>
         </ul>
       </nav>
