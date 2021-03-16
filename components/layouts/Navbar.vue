a8237698f1173ae5c8ae7712d6a615aa422c3520
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-16 11:00:43 +0800

    add icons in dropdown list

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -17,7 +17,10 @@
               <div class="absolute bg-gray-900 w-44 shadow-lg mt-4 rounded-sm right-0" v-show="dropdown">
                 <ul class="flex flex-col items-center p-2">
                   <li v-for="(droplink, i) in dropdownLink" :key="i" class="flex w-44 py-2 px-2">
-                    <nuxt-link class="w-full font-semibold ml-2 text-sm text-white" :to="droplink.goto">{{ droplink.title }}</nuxt-link>
+                    <nuxt-link class="flex w-full font-semibold ml-2 text-sm text-white" :to="droplink.goto">
+                      <span class="fill-current text-white w-5 mr-2" v-html="droplink.icon"></span>
+                      {{ droplink.title }}
+                      </nuxt-link>
                   </li>
                 </ul>
               </div>
@@ -37,10 +40,12 @@ export default {
       dropdownLink: [
         {
           title: 'Settings',
-          goto: '/settings'
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>',
+          goto: '/'
         },
         {
           title: 'Logout',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16 13v-4l8 7-8 7v-4h-6v-6h6zm0-6v-6h-16v18l8-7v-9h6v4h2z"/></svg>',
           goto: '/'
         }
       ]
