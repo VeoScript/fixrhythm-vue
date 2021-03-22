ed9ca327bc17dcf9dcd4b21019879efdef95f036
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-22 10:38:25 +0800

    add card layout (header)

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -5,6 +5,10 @@
     <div class="relative mx-10 my-20 w-full">
       <div class="absolute">
         <div><h1>This is Home</h1></div>
+      <div class="absolute w-full">
+       <div class="w-full px-3 py-3 rounded-md ring-1 ring-gray-500 bg-gray-200 text-gray-900">
+         <div class="c-head">Card Header</div>
+       </div>
       </div>
     </div>
   </div>
