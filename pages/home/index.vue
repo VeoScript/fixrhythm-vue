1403d54e87e10637fe169afa41efef4a36d09f40
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-22 10:53:55 +0800

    add card layout (body)

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,8 +4,11 @@
     <navbar />
     <div class="relative mx-5 my-20 w-screen">
       <div class="absolute w-full">
-       <div class="w-full px-3 py-3 rounded-md ring-1 ring-gray-500 bg-gray-200 text-gray-900">
-         <div class="c-head">Card Header</div>
+       <div class="w-full px-3 py-3 rounded-md ring-1 ring-gray-100 shadow-sm bg-gray-50 text-gray-900">
+         <div class="c-head"><nuxt-link to="/fprofile">Sparkle Script</nuxt-link></div>
+         <div class="c-body my-3">
+           <div class="p-title">Limitado</div>
+         </div>
        </div>
       </div>
     </div>
