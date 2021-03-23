c529d7a4a604dee8575c1eec41bdbc6a3d6e093e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-03-23 09:37:48 +0800

    add search input text

--- components/layouts/Navbar.vue
+++ components/layouts/Navbar.vue
@@ -6,6 +6,9 @@
           <li>
             <h5 class="md:ml-72 bg-red-600 px-3 py-1 rounded-full text-base text-center tracking-tight">{{ title }}</h5>
           </li>
+          <li class="hidden md:flex ml-2 w-full">
+            <input class="bg-gray-50 w-full text-black text-sm p-1 pl-3 rounded-full focus:outline-none" type="text" placeholder="Search">
+          </li>
           <li class="absolute flex md:hidden lg:hidden items-center inset-y-0 right-0 space-x-3">
             <nuxt-link class="flex" to="/profile">
               <img class="h-8 w-8 rounded-full ring-2 ring-gray-700" src="https://avatars.githubusercontent.com/u/26340308?s=460&u=f9dcea548505a99a2c873efb8b933aac0fe07abd&v=4" alt="account_avatar">
